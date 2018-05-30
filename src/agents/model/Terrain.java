01340cb6c19695395cad6931a4c6f408c29005ad
Cédric <zeptoline2@gmail.com>
2018-05-30 16:33:26 +0200

    Bonjour
    oui j'existe
    J'ai même fais un truc

---
+++ src/agents/model/Terrain.java
@@ -0,0 +1,16 @@
+package agents.model;
+
+import sim.engine.SimState;
+import sim.engine.Steppable;
+import metier.Dieu;
+import metier.Race;
+import metier.StrategieDeJeu;
+import sim.util.Int2D;
+
+public class Terrain {
+    private int typeTerrain;
+
+    public Terrain(int tt) {
+        typeTerrain = tt;
+    }
+}
